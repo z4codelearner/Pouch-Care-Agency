c6f1597ccefd92eaa1a9abba8bf1d0bf72ec31ea
Abu Zafor <z4codelearn@gmail.com>
2025-05-23 03:18:33 +0600

    Hero Section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // This code handles the click event for a mobile menu toggle button.
 // When the button is clicked, it toggles the visibility of the mobile menu by adding or removing the 'hidden' class.
+
     const toggleBtn = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
 
